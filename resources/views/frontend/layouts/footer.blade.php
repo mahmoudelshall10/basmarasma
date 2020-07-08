8fe0f18e72f2d385d2ddca311acd108f46e7b718
mahmoud <mahmoudelshall10@outlook.com>
2020-07-08 04:14:36 +0200

    second commit

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,93 @@
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-info">
+              
+            <img class="imageLogo" src="{{url('/frontend_design')}}/img/basma-and-rasma3.png" alt="basma&rasma">
+            <p>In alias aperiam. Placeat tempore facere. Officiis voluptate ipsam vel eveniet est dolor et totam porro.
+              Perspiciatis ad omnis fugit molestiae recusandae possimus. Aut consectetur id quis. In inventore
+              consequatur ad voluptate cupiditate debitis accusamus repellat cumque.</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Home</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">About us</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Services</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Home</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">About us</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Services</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              A108 Adam Street <br>
+              New York, NY 535022<br>
+              United States <br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+              <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="copyright">
+        &copy; Copyright <strong>Basma&Rasma</strong>. All Rights Reserved
+      </div>
+    </div>
+
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="{{url('/frontend_design')}}/lib/jquery/jquery.min.js"></script>
+  <script src="{{url('/frontend_design')}}/lib/jquery/jquery-migrate.min.js"></script>
+  <script src="{{url('/frontend_design')}}/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{url('/frontend_design')}}/lib/easing/easing.min.js"></script>
+  <script src="{{url('/frontend_design')}}/lib/superfish/hoverIntent.js"></script>
+  <script src="{{url('/frontend_design')}}/lib/superfish/superfish.min.js"></script>
+  <script src="{{url('/frontend_design')}}/lib/wow/wow.min.js"></script>
+  <script src="{{url('/frontend_design')}}/lib/venobox/venobox.min.js"></script>
+  <script src="{{url('/frontend_design')}}/lib/owlcarousel/owl.carousel.min.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="{{url('/frontend_design')}}/contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="{{url('/frontend_design')}}/js/main.js"></script>
+</body>
+
+</html>
---
+++ resources/views/frontend/layouts/nav.blade.php
@@ -0,0 +1,30 @@
+
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#intro">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#gallery">Image Gallery</a></li>
+          <li><a href="#activites">Activites</a>
+            <ul>
+              <li><a href="">ArtGallaries</a> </li>
+              <li><a href="">Workshops</a></li>
+              <li><a href="">Visits</a></li>
+              <li><a href="">Events</a></li>
+              <li><a href="">Graphity</a></li>
+            </ul>
+          </li>
+          <li><a href="#courses">Courses</a>
+            <ul>
+              <li><a href="">Drawing</a></li>
+              <li><a href="">Handmade</a></li>
+            </ul>
+          </li>
+          <li><a href="#handcrafts">Handcrafts</a></li>
+          <li><a href="#supporters">Sponsors</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li class="buy-tickets"><a href="#buy-tickets">Buy Tickets</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
